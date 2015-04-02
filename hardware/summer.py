05b07a1e387923c9a09e0d5c74dc0cdd66be52e8
rzoss <rico.zoss@gmail.com>
2015-04-02 16:58:34 +0200

    reformat files

--- hardware/summer.py
+++ hardware/summer.py
@@ -5,19 +5,20 @@ import RPi.GPIO as GPIO
 
 PIN_A = 12
 
-class summer:
-	def __init__(self):
-		# Initialisiere GPIO
-		# Verwendet physische pin numbern
-		GPIO.setmode(GPIO.BOARD)
-		GPIO.setup(PIN_A, GPIO.OUT)
 
-	def beep(self, zeit):
-		# Initialisiere PWM an pwmPin mit frequenz 4kHz
-		pwm = GPIO.PWM(PIN_A, 4000)
-		pwm.start(0)
-		# Tastgrad auf 50% einstellen
-		pwm.ChangeDutyCycle(50)
-		time.sleep(zeit)
-		pwm.stop()
+class summer:
+    def __init__(self):
+        # Initialisiere GPIO
+        # Verwendet physische pin numbern
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(PIN_A, GPIO.OUT)
+
+    def beep(self, zeit):
+        # Initialisiere PWM an pwmPin mit frequenz 4kHz
+        pwm = GPIO.PWM(PIN_A, 4000)
+        pwm.start(0)
+        # Tastgrad auf 50% einstellen
+        pwm.ChangeDutyCycle(50)
+        time.sleep(zeit)
+        pwm.stop()
 
