0e8f0a37774c43465f5d7ec97c676ffc6b013142
Rico Zoss <rico.zoss@gmail.com>
2015-04-03 10:33:04 +0200

    fixed format

--- hardware/portexpander.py
+++ hardware/portexpander.py
@@ -38,99 +38,97 @@ class PCF8574:
         # LED ist an IC3 (adresse 0x21)
         result, buf = ci2c.read(0x21, 1)
         data = buf[0]
+        # Aktueller zustand der LED's abfragen
+        data &= 0xFE
+        data = state | data
+        # Zustand der Roten LED aktualisieren
+        ci2c.write(0x21, [data])
 
-    # Aktueller zustand der LED's abfragen
-    data &= 0xFE
-    data = state | data
-    # Zustand der Roten LED aktualisieren
-    ci2c.write(0x21, [data])
-
-
-# Zustand des Gruenen LED festlegen
-def setGreenLED(self, state):
-    # LED ist an IC3 (adresse 0x21)
-    state = state << 1
-    result, buf = ci2c.read(0x21, 1)
-    data = buf[0]
-    # Aktueller zustand der LED's abfragen
-    data = data & 0xFD
-    data = state | data
-    # Zustand der Roten LED aktualisieren
-    ci2c.write(0x21, [data])
+    # Zustand des Gruenen LED festlegen
+    def setGreenLED(self, state):
+        # LED ist an IC3 (adresse 0x21)
+        state = state << 1
+        result, buf = ci2c.read(0x21, 1)
+        data = buf[0]
+        # Aktueller zustand der LED's abfragen
+        data = data & 0xFD
+        data = state | data
+        # Zustand der Roten LED aktualisieren
+        ci2c.write(0x21, [data])
 
 
-# Zustand der LED's festlegen
-def setBothLED(self, state_Rot, state_Gruen):
-    # LED's sind an IC3 (adresse 0x21)
-    state_Gruen = state_Gruen << 1
-    data = state_Rot | 0xFC
-    data = state_Gruen | data
-    # Zustand der LED's aktualisieren
-    ci2c.write(0x21, [data])
+    # Zustand der LED's festlegen
+    def setBothLED(self, state_Rot, state_Gruen):
+        # LED's sind an IC3 (adresse 0x21)
+        state_Gruen = state_Gruen << 1
+        data = state_Rot | 0xFC
+        data = state_Gruen | data
+        # Zustand der LED's aktualisieren
+        ci2c.write(0x21, [data])
 
 
-# Zustand der Rueck Taste auslesen
-def readBack(self):
-    # Taste ist an IC5 (adresse 0x20)
-    result, buf = ci2c.read(0x20, 1)
-    data = buf[0]
-    data = data ^ 0xFF
-    # Rückgabe des Tasten Zustandes
-    if data & 0x10:
-        return True
-    else:
-        return False
+    # Zustand der Rueck Taste auslesen
+    def readBack(self):
+        # Taste ist an IC5 (adresse 0x20)
+        result, buf = ci2c.read(0x20, 1)
+        data = buf[0]
+        data = data ^ 0xFF
+        # Rückgabe des Tasten Zustandes
+        if data & 0x10:
+            return True
+        else:
+            return False
 
 
-# Zustand der OK Taste auslesen
-def readOK(self):
-    # Taste ist an IC5 (adresse 0x20)
-    result, buf = ci2c.read(0x20, 1)
-    data = buf[0]
-    data = data ^ 0xFF
-    # Rückgabe des Tasten Zustandes
-    if data & 0x20:
-        return True
-    else:
-        return False
+    # Zustand der OK Taste auslesen
+    def readOK(self):
+        # Taste ist an IC5 (adresse 0x20)
+        result, buf = ci2c.read(0x20, 1)
+        data = buf[0]
+        data = data ^ 0xFF
+        # Rückgabe des Tasten Zustandes
+        if data & 0x20:
+            return True
+        else:
+            return False
 
 
-# Zustand der Vor Taste auslesen
-def readNext(self):
-    # Taste ist an IC5 (adresse 0x20)
-    result, buf = ci2c.read(0x20, 1)
-    data = buf[0]
-    data = data ^ 0xFF
-    # Rückgabe des Tasten Zustandes
-    if data & 0x40:
-        return True
-    else:
-        return False
+    # Zustand der Vor Taste auslesen
+    def readNext(self):
+        # Taste ist an IC5 (adresse 0x20)
+        result, buf = ci2c.read(0x20, 1)
+        data = buf[0]
+        data = data ^ 0xFF
+        # Rückgabe des Tasten Zustandes
+        if data & 0x40:
+            return True
+        else:
+            return False
 
 
-# Zustand der Tasten auslesen
-# Rueckegabe Rueck_Taste 0x10
-# Rueckegabe OK_Taste 0x20
-# Rueckegabe Vor_Taste 0x40
-def readButton(self):
-    # Tasten sind an IC5 (adresse 0x20)
-    result, buf = ci2c.read(0x20, 1)
-    data = buf[0]
-    # Rückgabe der Tasten Zustaende
-    data = data ^ 0xFF
-    if data & 0x10:
-        return 0x01
-    if data & 0x20:
-        return 0x02
-    if data & 0x40:
-        return 0x04
-    else:
-        return 0x00
+    # Zustand der Tasten auslesen
+    # Rueckegabe Rueck_Taste 0x10
+    # Rueckegabe OK_Taste 0x20
+    # Rueckegabe Vor_Taste 0x40
+    def readButton(self):
+        # Tasten sind an IC5 (adresse 0x20)
+        result, buf = ci2c.read(0x20, 1)
+        data = buf[0]
+        # Rückgabe der Tasten Zustaende
+        data = data ^ 0xFF
+        if data & 0x10:
+            return 0x01
+        if data & 0x20:
+            return 0x02
+        if data & 0x40:
+            return 0x04
+        else:
+            return 0x00
 
 
-# wait until a button was pressed
-def waitForButton(self):
-    while not self.readButton():
-        time.sleep(CFG_BUTTON_PRESSED_POLL_TIME)
-    return self.readButton()
+    # wait until a button was pressed
+    def waitForButton(self):
+        while not self.readButton():
+            time.sleep(CFG_BUTTON_PRESSED_POLL_TIME)
+        return self.readButton()
 			
